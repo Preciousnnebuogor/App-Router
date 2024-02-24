2af28fba095176cf6ae1e4ef3a4681d47300e84c
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-25 00:09:21 +0100

    added the submit button

--- src/app/navbar/name.tsx
+++ src/app/navbar/name.tsx
@@ -8,12 +8,13 @@ export default function Name() {
         <p>Enter the name you use in real life</p>
       </div>
       <div className={style.input}>
-      <input type="text" placeholder="First name" />
-      <input type="text" placeholder="Middle name" />
-      <input type="text" placeholder="Surname" />
-      <br/>
-      <button className={style.button}>Next</button>
+        <input type="text" placeholder="First name" />
+        <input type="text" placeholder="Middle name" />
+        <input type="text" placeholder="Surname" />
+        <br />
+        <button className={style.button}>Next</button>
       </div>
+
       <h3>I already have an account</h3>
     </div>
   );
