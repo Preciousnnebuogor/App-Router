bc1a28231f11aaf8856c99b0be47053ca0fffdb7
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-25 21:32:45 +0100

    responsiveness added

--- src/app/navbar/name.tsx
+++ src/app/navbar/name.tsx
@@ -1,9 +1,23 @@
 "use client";
-import {useRouter} from "next/navigation"
+import { useRouter } from "next/navigation";
 import style from "./name.module.scss";
+import { useState } from "react";
 export default function Name() {
-    
-    const router = useRouter()
+  const router = useRouter();
+  const [data, setData] = useState({
+    firstname: "",
+    middlename: "",
+    surname: "",
+  });
+  const handleChange = (e:any) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form hs been submitted")
+}
 
   return (
     <div className={style.body}>
@@ -11,12 +25,37 @@ export default function Name() {
         <h1>What's your name?</h1>
         <p>Enter the name you use in real life</p>
       </div>
-      <div className={style.input}>
-        <input type="text" placeholder="First name" />
-        <input type="text" placeholder="Middle name" />
-        <input type="text" placeholder="Surname" />
+      <div className={style.input} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstname"
+          value={data.firstname}
+          onChange={handleChange}
+          placeholder="First name"
+        />
+        <input
+          type="text"
+          name="middlename"
+          value={data.middlename}
+          onChange={handleChange}
+          placeholder="Middle name"
+        />
+        <input
+          type="text"
+          name="surname"
+          value={data.surname}
+          onChange={handleChange}
+          placeholder="Surname"
+        />
         <br />
-        <button className={style.button} onClick={()=> {router.push("/page")}}>Next</button>
+        <button
+          className={style.button}
+          onClick={() => {
+            router.push("/page");
+          }}
+        >
+          Next
+        </button>
       </div>
 
       <h3>I already have an account</h3>
