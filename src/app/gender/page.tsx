058a37f2945a23596c365badc6bd2e9fd3db6371
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-27 15:28:48 +0100

    added the radio option to one click

--- src/app/gender/page.tsx
+++ src/app/gender/page.tsx
@@ -6,30 +6,44 @@ export default function Gender() {
 
     const router = useRouter()
 
-    const [data,setData] = useState()
+    const [gender,setGender] = useState({});
+
+    const handleChange = (e:any)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setGender({...gender, [name]: value})
+    }
+
   return (
     <div className={style.body}>
       <div className={style.text}>
         <h1>What's your gender?</h1>
         <h4>You can change who see your gender on your profile later.</h4>
       </div>
+
       <div className={style.table}>
         <div className={style.form}>
           <p>Female</p>
-          <input type="radio" />
+          <input type="radio" name="gender" value="female"
+           onChange={handleChange}/>
         </div>
+
         <div className={style.form}>
           <p>Male</p>
-          <input type="radio" />
+          <input type="radio" name="gender" value="male"
+           onChange={handleChange} />
         </div>
+
         <h3>More options</h3>
         <div className={style.form}>
           <h4 className={style.select}>
             Select "More options" to choose another gender or if you'd rather
             not say.
           </h4>
-          <input type="radio" />
+          <input type="radio" name="gender" value="other"
+           onChange={handleChange} />
         </div>
+
       </div>
 
       <button className={style.button} onClick={() => {router.push("/contact")}}>Next</button>
