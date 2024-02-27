72347d7b05983dd75c8600c796357cc31893d8a1
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-27 20:53:42 +0100

    added css to save folder

--- src/app/save/page.tsx
+++ src/app/save/page.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+import style from "./save.module.scss";
+
 export default function Save() {
-    return <div>
+    return <div className={style.body}>
         <div>
             <h1>Save your login info?</h1>
             <p>We'll save the login info for name displayed, so you won't need to enter it next time you log in.</p>
         </div>
-        <div>
-            <button></button>
-            <button></button>
+        <div className={style.buttona}>
+            <button className={style. button1}>Save</button>
+            <br/>
+            <button className={style.button2}><Link href="/">Not now</Link></button>
         </div>
     </div>
 }
