20e69b1c8563dc46cfd771a01aaab6209d838f01
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-02-28 22:38:24 +0100

    onSubmitHandler responsive

--- src/app/navbar/name.tsx
+++ src/app/navbar/name.tsx
@@ -6,31 +6,20 @@ import { Schema } from "./schema";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 export default function Name() {
+  const router = useRouter();
+
+  const [data, setData] = useState();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm({ resolver: yupResolver(Schema) });
-  const onSubmitHandler = (data:any) => {
-    console.log({data});
-  }
-  const router = useRouter();
-  const [data, setData] = useState({
-    firstname: "",
-    middlename: "",
-    surname: "",
-    ValidationSchema: Schema,
-  });
-  // const handleChange = (e: any) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setData({ ...data, [name]: value });
-  // };
-  // const handleSubmit = (e:any) => {
-  //   e.preventDefault();
-  //   console.log("Form hs been submitted");
-  // };
+  const onSubmitHandler = (data: any) => {
+    console.log({ data });
+    router.push("/date")
+  };
 
   return (
     <div className={style.body}>
@@ -38,18 +27,18 @@ export default function Name() {
         <h1>What's your name?</h1>
         <p>Enter the name you use in real life</p>
       </div>
-      <div className={style.input} onSubmit={handleSubmit(onSubmitHandler)}>
+      <form className={style.input} onSubmit={handleSubmit(onSubmitHandler)}>
         <input
           type="text"
           required
           placeholder="First name"
-          {...register("firstname")} 
+          {...register("firstname")}
         />
         <p>{errors.firstname?.message}</p>
         <input
           type="text"
           required
-          {...register("middlename")} 
+          {...register("middlename")}
           placeholder="Middle name"
         />
         <p>{errors.middlename?.message}</p>
@@ -57,16 +46,17 @@ export default function Name() {
           type="text"
           required
           placeholder="Surname"
-          {...register("surname")} 
+          {...register("surname")}
         />
         <p>{errors.surname?.message}</p>
         <br />
         <button
           className={style.button}
+          type="submit"
         >
           Next
         </button>
-      </div>
+      </form>
     </div>
   );
 }
